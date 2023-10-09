40322972b87fae5c6fb86412191294016e042105
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-10-09 14:50:09 -0300

    initial commit

---
+++ src/main/java/com/healthnove/schedulingHealthNove/SchedulingHealthNoveApplication.java
@@ -0,0 +1,13 @@
+package com.healthnove.schedulingHealthNove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingHealthNoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingHealthNoveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/healthnove/schedulingHealthNove/SchedulingHealthNoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthnove.schedulingHealthNove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulingHealthNoveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
