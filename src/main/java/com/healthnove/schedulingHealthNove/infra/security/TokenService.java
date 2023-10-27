e3679af100525425ef977eb1e5e0b400f86a21db
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-10-27 17:11:05 -0300

    feat: security configurations

---
+++ src/main/java/com/healthnove/schedulingHealthNove/infra/security/TokenService.java
@@ -0,0 +1,56 @@
+package com.healthnove.schedulingHealthNove.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.healthnove.schedulingHealthNove.domain.model.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("HealthNove API")
+                    .withSubject(user.getUsername())
+                    .withClaim("id", user.getId())
+                    .withClaim("name", user.getFirstName())
+                    .withExpiresAt(expirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error generating JWT token", exception);
+        }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("HealthNove API")
+                    .build()
+                    .verify(tokenJWT)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token JWT inválido ou expirado!");
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+}
