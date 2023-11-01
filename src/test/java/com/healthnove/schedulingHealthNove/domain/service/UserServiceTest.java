3d06f5d1719df3ba537734d91e280091e2a827db
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-11-01 17:07:41 -0300

    refactor: user service test

--- src/test/java/com/healthnove/schedulingHealthNove/domain/service/UserServiceTest.java
+++ src/test/java/com/healthnove/schedulingHealthNove/domain/service/UserServiceTest.java
@@ -9,12 +9,11 @@ import com.healthnove.schedulingHealthNove.domain.exception.UserNotFoundExceptio
 import com.healthnove.schedulingHealthNove.domain.model.User;
 import com.healthnove.schedulingHealthNove.domain.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -30,24 +29,20 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+
+@SpringBootTest
 class UserServiceTest {
 
     private static final Long ID = 1L;
 
-
     @InjectMocks
-    UserService userService;
+    private UserService userService;
 
     @Mock
     private UserRepository repository;
 
     @Mock
     private PasswordEncoder passwordEncoder;
-
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.openMocks(this);
-    }
 
     @Test
     void shouldReturnUserResponseDto_whenCreateCorrectly() {
@@ -59,9 +54,6 @@ class UserServiceTest {
         when(passwordEncoder.encode(any())).thenReturn("$2a$10$J76/zwJqHkFgKFJi8wWJTuwjYDX8U0elOJVGBVNBwGv2to5nggZHa");
 
         UserResponseDto userResponse = userService.create(userRequest);
-
-        System.out.println("Esperado" + expectedResponse);
-        System.out.println("Recebido" + userResponse);
 
         assertEquals(expectedResponse, userResponse);
     }
