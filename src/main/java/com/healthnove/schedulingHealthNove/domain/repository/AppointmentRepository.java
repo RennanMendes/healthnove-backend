dcfddf9ffcc23d9915397b3939acecaba2f16d8a
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-11-19 13:04:28 -0300

    refactor: search for scheduled appointment

--- src/main/java/com/healthnove/schedulingHealthNove/domain/repository/AppointmentRepository.java
+++ src/main/java/com/healthnove/schedulingHealthNove/domain/repository/AppointmentRepository.java
@@ -3,6 +3,8 @@ package com.healthnove.schedulingHealthNove.domain.repository;
 import com.healthnove.schedulingHealthNove.domain.enumerated.Status;
 import com.healthnove.schedulingHealthNove.domain.model.Appointment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -16,7 +18,10 @@ public interface AppointmentRepository extends JpaRepository<Appointment, Long> 
 
     List<Appointment> findByDoctorIdAndStatus(Long id, Status status);
 
-    Boolean existsByUserIdAndDateBetween(Long id, LocalDateTime firstHour, LocalDateTime lastHour);
+    @Query("SELECT EXISTS (SELECT 1 FROM Appointment a WHERE a.doctor.id = :id AND a.date = :date AND a.status = 'SCHEDULED')")
+    boolean existsByDoctorIdAndDate(@Param("id") Long id, @Param("date") LocalDateTime date);
 
-    Boolean existsByDoctorIdAndDate(Long id, LocalDateTime date);
+    @Query("SELECT EXISTS (SELECT 1 FROM Appointment a WHERE a.user.id = :userId AND a.status = SCHEDULED AND a.date BETWEEN :firstHour AND :lastHour)")
+    boolean existsByUserIdAndDateBetween(@Param("userId") Long userId,  @Param("firstHour") LocalDateTime firstHour, @Param("lastHour") LocalDateTime lastHour);
+
 }
